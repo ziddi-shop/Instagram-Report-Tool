8418f162770f2f6854f499027f4d2df564464c8b
ZIDDI_SHOP <155100438+ziddi-shop@users.noreply.github.com>
2025-09-24 00:38:21 +0530

    ᴢɪᴅᴅɪ

---
+++ libs/logo.py
@@ -0,0 +1,28 @@
+
+from colorama import Fore, Back, Style
+from random import choice
+
+logo = """
+
+███╗░░░███╗███████╗████████╗░█████╗░  ░██████╗███████╗██████╗░██╗░░░██╗███████╗██████╗░
+████╗░████║██╔════╝╚══██╔══╝██╔══██╗  ██╔════╝██╔════╝██╔══██╗██║░░░██║██╔════╝██╔══██╗
+██╔████╔██║█████╗░░░░░██║░░░███████║  ╚█████╗░█████╗░░██████╔╝╚██╗░██╔╝█████╗░░██████╔╝
+██║╚██╔╝██║██╔══╝░░░░░██║░░░██╔══██║  ░╚═══██╗██╔══╝░░██╔══██╗░╚████╔╝░██╔══╝░░██╔══██╗
+██║░╚═╝░██║███████╗░░░██║░░░██║░░██║  ██████╔╝███████╗██║░░██║░░╚██╔╝░░███████╗██║░░██║
+╚═╝░░░░░╚═╝╚══════╝░░░╚═╝░░░╚═╝░░╚═╝  ╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝ """
+
+urls = [
+    "GitHub - https://github.com/ziddi-shop",
+    "Instagram - https://instagram.com/yknowxziddi",
+    "Telegram - https://t.me/meta_server",
+    "Telegram - https://t.me/nobi_shops",
+    "InstaReporter Tool - https://github.com/ziddi-shop/Instagram-Report-Tool",
+    "Gmail - mailto:shehzadakingziddi.com"
+    ]
+
+def print_logo():
+    print(Fore.RED + Style.BRIGHT + logo + Style.RESET_ALL + Style.BRIGHT +"\n")
+    print(Fore.MAGENTA + "      Producer: Muneeb"+ Style.RESET_ALL + Style.BRIGHT)
+    print(Fore.CYAN + "\n", "-> Follow me On Instagram @yknowxziddi.")
+    print ("\n", "-> Meta Server:\n    " + choice(urls))
+    print(Style.RESET_ALL + Style.BRIGHT, Style.BRIGHT)
