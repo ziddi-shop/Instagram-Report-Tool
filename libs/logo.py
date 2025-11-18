0533d0f52e8a98be27a9b2cf8ad2dfff22a157d3
ZIDDI SHOP <155100438+ziddi-shop@users.noreply.github.com>
2025-11-18 17:43:56 +0530

    Update logo.py

--- libs/logo.py
+++ libs/logo.py
@@ -22,7 +22,7 @@ urls = [
 
 def print_logo():
     print(Fore.RED + Style.BRIGHT + logo + Style.RESET_ALL + Style.BRIGHT +"\n")
-    print(Fore.MAGENTA + "      Producer: Muneeb"+ Style.RESET_ALL + Style.BRIGHT)
+    print(Fore.MAGENTA + "      Producer: ZIDDI"+ Style.RESET_ALL + Style.BRIGHT)
     print(Fore.CYAN + "\n", "-> Follow me On Instagram @yknowxziddi.")
     print ("\n", "-> Meta Server:\n    " + choice(urls))
     print(Style.RESET_ALL + Style.BRIGHT, Style.BRIGHT)
